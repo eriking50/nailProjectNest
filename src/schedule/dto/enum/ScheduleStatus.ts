c4e8ff3f912312583f739c75e35ee3d6ffb77047
eriking50 <eriking5@hotmail.com>
2022-03-15 23:35:36 -0300

    refactor/delete enums not used

--- src/schedule/dto/enum/ScheduleStatus.ts
+++
@@ -1,6 +0,0 @@
-export enum ScheduleStatus {
-  SCHEDULED = 0,
-  RESCHEDULED = 1,
-  PAID = 2,
-  DEBT = 3,
-}
--- src/schedule/dto/enum/ScheduleType.ts
+++
@@ -1,8 +0,0 @@
-export enum ScheduleType {
-  HANDS = 0,
-  FEET = 1,
-  HANDSFEET = 2,
-  GELNAIL = 3,
-  FIX = 4,
-  FEETSPA = 5,
-}
