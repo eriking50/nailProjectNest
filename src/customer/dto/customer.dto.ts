868233b714e5a91afb520d7666e757cf4d9470cd
eriking50 <eriking5@hotmail.com>
2022-03-15 23:21:37 -0300

    feat/add validation pipe and validations to create routes

--- src/customer/dto/customer.dto.ts
+++ src/customer/dto/customer.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CustomerDTO {
+  @IsNotEmpty({
+    message: 'O nome do cliente não pode ser vazio',
+  })
   name: string;
+
+  @IsNotEmpty({
+    message: 'O telefone do cliente não pode ser vazio',
+  })
   phone: string;
 }
--- src/schedule/dto/schedule.dto.ts
+++ src/schedule/dto/schedule.dto.ts
@@ -1,10 +1,42 @@
 import { Status, Type } from '@prisma/client';
+import { IsDateString, IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class ScheduleDTO {
+  @IsDateString(
+    {},
+    {
+      message: 'Informe uma data válida',
+    },
+  )
   scheduleDate: Date;
+
+  @IsNotEmpty({
+    message: 'O título não pode ser vazio',
+  })
   title: string;
+
+  @IsNumber(
+    {},
+    {
+      message: 'Informe um preço usando um tipo number',
+    },
+  )
   price: number;
+
+  @IsEnum(Type, {
+    message:
+      "Informe o tipo considerando as seguintes opções: 'HANDS','FEET','HANDSFEET','GELNAIL','FIX','FEETSPA'",
+  })
   type: Type;
+
+  @IsEnum(Status, {
+    message:
+      "Informe o tipo considerando as seguintes opções: 'SCHEDULED','RESCHEDULED','PAID','DEBT'",
+  })
   status: Status;
+
+  @IsNotEmpty({
+    message: 'O id do cliente não pode ser vazio',
+  })
   customerId: string;
 }
